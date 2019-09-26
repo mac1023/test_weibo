c2830f97253d879425aedf3a326cec098edb857a
zhangyajie <z_yajie@sina.com>
2019-09-26 09:43:27 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,10 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
+
+
+//会话管理
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
